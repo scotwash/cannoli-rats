6e86e5ee814c2325fbe12defa9871b73ce82fffd
Scotch Washington <milanwashington54@gmail.com>
2025-12-12 22:12:46 -0500

    dadadada

--- cannoli-rats/Classes/Player_Script.gd
+++ cannoli-rats/Classes/Player_Script.gd
@@ -1,4 +1,6 @@
-extends Resource
+extends Node
 class_name PlayerStats
 # coins
 @export var playerCoins : int = 0
+# tips 
+@export var playerTips : int = 0
--- cannoli-rats/Scenes/test_main.gd
+++ cannoli-rats/Scenes/test_main.gd
@@ -1,6 +1,15 @@
 extends Node2D
-# call rat class
+	# call rat class
 var newRat = RatNPC.new()
 #call player class 
-
+var newPlayer = PlayerStats.new()
+func _ready() -> void:
+	return
 # call player hud
+
+#save player data
+func _savedata():
+	return
+func _init() -> void:
+	return
+	
