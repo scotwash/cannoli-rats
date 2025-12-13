8e17fb79dd843ac5ecfdb5523e878b555c4db9e9
Scotch Washington <milanwashington54@gmail.com>
2025-12-12 21:04:16 -0500

    Fixed Singelton eerrot

--- cannoli-rats/UI/dialogue_txt.gd
+++ cannoli-rats/UI/dialogue_txt.gd
@@ -1,11 +1,11 @@
 extends RichTextLabel
-
+# ANIMATED TEXT
+@onready var rich_label = %DialogueTxt
 
 func _ready() -> void:
 	var content = FileAccess.open("res://Text Files/test_order.txt", FileAccess.READ)
-	var content_text = content.get_as_text()
-	%DialogueTxt.text = content_text
-	
+	%DialogueTxt.text = content.get_as_text()
+
 	
 	
 	
